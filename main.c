5bfa05c62a92516906fcbdcd46a51bc27fe9261d
ItoLucca <161767087+ItoLucca@users.noreply.github.com>
2024-03-11 21:35:55 +0000

    testando

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 #include "atividade.h"
 
 int main(){
-    printf("Bora");
+    int *ultima_posicao, opcao;
+    int lista_soma[*ultima_posicao];
+
+
+    if(opcao = 1){
+        printf("Digite um numero para que seja realizada a soma: \n");
+        scanf(*ultima_posicao);
+    }
+
+    else{
+        printf("A soma é igual a: %i", &lista_soma);
+    }
+
 
     return 0;
 }
